02eabc39e1c84c18d71eb5dd6ab332688f42b84f
AntoniuStirb <67384698+AntoniuStirb@users.noreply.github.com>
2023-01-18 01:18:51 +0200

    Add files via upload

--- Solver/Solver.go
+++ Solver/Solver.go
@@ -2,6 +2,7 @@ package Solver
 
 import (
 	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
 )
@@ -28,52 +29,96 @@ func DeleteInvalidLines(records [][]string) [][]string {
 		for j := 0; j < len(records[i]); j++ {
 			if records[i][j] == "" {
 				records = append(records[:i], records[i+1:]...) //stergem recordul cu indexul dorit
+				i--                                             //avem unele date care au invalid si email si gender de ex.
 			}
 		}
 	}
 	return records
 }
 
-func CreateNewCsv(records [][]string, fileNumber int) {
-	csvfile, err := os.Create("chunkFile.csv")
-	if err != nil {
-		log.Fatal("Failed to create the file: %s", err)
+//func CreateNewCsv(fNumber int) {
+//	fName := fmt.Sprintf("file%v", fNumber)
+//	csvfile, err := os.Create(fName)
+//	if err != nil {
+//		log.Fatal("Failed to create the file: %s", err)
+//	}
+//	cswriter := csv.NewWriter(csvfile)
+//}
+
+func WriteInFiles(records [][]string, chunkSize int) {
+	var numberOfFiles int
+	if len(records)%chunkSize == 0 {
+		numberOfFiles = len(records) / chunkSize
+	} else {
+		numberOfFiles = len(records)/chunkSize + 1
 	}
 
-	cswriter := csv.NewWriter(csvfile)
+	for j := 0; j < numberOfFiles; j++ {
+		fName := fmt.Sprintf("file%v", j)
+		csvfile, err := os.Create(fName)
+		if err != nil {
+			log.Fatal("Failed to create the file: %s", err)
+		}
+		cswriter := csv.NewWriter(csvfile)
+		_ = cswriter.Write(records[0])
 
-	for _, records := range records {
-		_ = cswriter.Write(records)
+		var lastItemIndex int // we have 927 elements and to avoid going to 1000 last index
+		if j == numberOfFiles-1 {
+			lastItemIndex = len(records) - 1
+		} else {
+			lastItemIndex = (j + 1) * chunkSize
+		}
+
+		for i := j*chunkSize + 1; i <= lastItemIndex; i++ {
+			_ = cswriter.Write(records[i])
+			cswriter.Flush()
+		}
+
 	}
 
-	cswriter.Flush()
-	csvfile.Close()
 }
 
 //func SplitInChunks(records [][]string, chunkDimension int) {
 //	fileLength := ((len(records)-1))/chunkDimension + 1)
 //}
 
-func ChunkSlice(records [][]string, chunkDimension int) [][]string {
-	var chunks [][]string
-	for {
-		if len(records) == 0 {
-			break
-		}
+//func ChunkSlicer(slice []string, chunkSize int) [][]string {
+//	var chunks [][]string
+//	for i := 0; i < len(slice); i += chunkSize {
+//		end := i + chunkSize
+//
+//		// necessary check to avoid slicing beyond
+//		// slice capacity
+//		if end > len(slice) {
+//			end = len(slice)
+//		}
+//
+//		chunks = append(chunks, slice[i:end])
+//	}
+//
+//	return chunks
+//}
 
-		// necessary check to avoid slicing beyond
-		// slice capacity
-		if len(records) < chunkDimension {
-			chunkDimension = len(records)
-		}
-
-		chunks = append(chunks, records[0:chunkDimension]...)
-		
-		records = records[chunkDimension:]
-	}
-
-	return chunks
-}
+//func ChunkSlice(records [][]string, chunkDimension int) [][]string {
+//	var chunks [][]string
+//	for {
+//		if len(records) == 0 {
+//			break
+//		}
+//
+//		// necessary check to avoid slicing beyond
+//		// slice capacity
+//		if len(records) < chunkDimension {
+//			chunkDimension = len(records)
+//		}
+//
+//		chunks = append(chunks, records[0:chunkDimension]...)
+//
+//		records = records[chunkDimension:]
+//	}
+//
+//	return chunks
+//}
 
 //func DeleteElement(slice [][]string, index int) [][]string {
 //	return append(slice[:index], slice[index+1:]...)
--- main/main.go
+++ main/main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"Project1GO/Solver"
-	"fmt"
 )
 
 func main() {
 
-	//chunkDimension := 10
-
+	chunkDimension := 100
 	records := Solver.DeleteInvalidLines(Solver.ReadCsv("input.csv"))
+	//fmt.Println(len(records))
 
-	fmt.Println(records)
-
-	fmt.Println(records[3][4] == "")
-	fmt.Println(records[2])
-
-	fmt.Println(len(records))
-
+	//fmt.Println(records)
+	//
+	//fmt.Println(records[3][4] == "")
+	//fmt.Println(records[2])
+	//
+	//fmt.Println(len(records))
+	//fmt.Println(len(records[0]))
 	//Solver.CreateNewCsv(records)
 
-	fmt.Println(Solver.ChunkSlice(records, 2))
+	//for i := 0; i < len(records); i++ {
+	//	slicer := Solver.ChunkSlicer(records[i], 4)
+	//	fmt.Println("==========", slicer, "===========")
+	//}
+
+	Solver.WriteInFiles(records, chunkDimension)
 
 }
